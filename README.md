961f42705797f63d3533334af20fd7fd0f7b7c07
ashleyborum <ashb4101@gmail.com>
2023-06-14 12:07:25 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# World_Space_Frontend
+## World Space GUI
+
+https://user-images.githubusercontent.com/76723573/232329859-fb4b8e3f-2004-46cf-bb8e-59d824cbf417.mov
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Runs the app in the development mode.\
+
+Opens in native browser window
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
---
+++ build/electron.js
@@ -0,0 +1,44 @@
+// ./public/electron.js
+const path = require('path');
+const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. 
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
